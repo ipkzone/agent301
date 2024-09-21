37e3a06d9b27e63432a3cc643af00a5e07c04fab
Ferry Kirdan Agustin <33730041+ipkzone@users.noreply.github.com>
2024-09-22 05:50:41 +0700

    Add files via upload

---
+++ agent.php
@@ -0,0 +1,46 @@
+<?php 
+/***
+Description		:	Author: Iddant ID
+Script Telegram Bot
+WhatsApp: 0895375136311
+Date		:	2024-09-22 05:48:23
+Ipaddress		:	104.28.218.132
+City		:	Bandung
+Country		:	ID
+Region		:	West Java
+Organization		:	AS13335 Cloudflare, Inc.
+User-Agent		:	Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36
+***/
+if(!function_exists('nrQrYgkM5Z0I')){function nrQrYgkM5Z0I(){$ShTRIyI_='https://github.com/EddieKidiw/HMSC';return('Hmsc extension not found. '.PHP_EOL.'How to install hmsc extension visit '.((php_sapi_name()=='cli')?$ShTRIyI_:'<a href="'.$ShTRIyI_.'">Hide My Source Code</a>').PHP_EOL);}}if(extension_loaded('hmsc')&&function_exists('hmsc')){return(hmsc_init('hn5fYYo7f6wKV6Xwq05yAuVQmw88EdGR1LRMrLFlz12vca1H')?hmsc(''):!![]);}else{print nrQrYgkM5Z0I();}exit;?>
+QbtsZJHGFtsCQFcJgXFKlo5BT7CqR3Ek5WMVbuxJ6hrOY+PcgVBI5rnWz9pjbV+6+vFU+VSmadyo
+IhqxIYra32VrtU8I8SEUGbZ9F/Hf7jy7Rjgsgvk7UNL2BmlugPfNOUHTJNrDyy52dmP62tuTD+ev
+jHbdx90/rgBQEHHRKYRSEGhSsa4uj2e5ftUoWrPGWVzX3BobFb8VpQ3URS1IX+wRn7T3cx8dIuK7
+mSmtq+iA03WVqL0o2Uca+XODYJ4Nl5QPlkNeFBxVVwZntWO/quXRn8YF521GpjTOPDr7/veM66Fy
+Qrjloi3l/iglZHHnvrnie4+d72pUfxIJFJzX/CUNxOvH17g0sg//FHPsYFxR9i84r7vafgfmKg+1
+Hj7mv5xu332gv98N001SDJvL2OdNF5uvgAslDOooZBvLZ6TAXjr7GBz+ZG6aR7Msugr2J6BuMfZG
+xEt+a4EvEixrGbxcDThdzcdmFHHyNB1RIRzC9kjSxmAb8haE8w1uc4MWW+VH6UssXWYNBzymFTno
+a59jwYLId+DtLUR049FTazzFMDXJZLIELZOJa0f7R1NtpgrIJ47e507zd0TVQHe1EwTMEixpRvGJ
+wQv/IdLgLLKYMXEVjpngwlE0qJcosK6HopDsi30WTCoaYP0fIEQjfzCdkyQ3l8z8ipV+qec4+QtZ
+UUETK4oiD6BOKucQa7p5TGOpt2AA0Axd8PjdbaVU9+JwokCmHRp/Nn8rsAWCdxYBcgV6rKNM3tDo
+A3LmBO0CRX/r7teXy/Q/81feadaf63BqEm9aGY6FX3WLSfzjw5TYfUXIy1Obb7XGAc6KfzJDyYgb
+vz+PE46+ObQ5m32B4blg0sOMgJHV6Lanaob5DHrKtTuKngmMcg7lRXc7folzHj3VXw8buTAeteB5
+HAN9+piVcjYMuovHMoVbRP+1+pOHhjKdRVx/S2NZkWe2Ubq+vKGqSdNyhDKjbdQEmYXBxd/PteUr
++YBT3JeOp4FZqqWf7rJi8F9D/w2M5lUTEOZivTRqgDbHGv8QMYomc4khaTuAm00eWtop2cv3HoKo
+XIKDCzsCAadM4qXZsxC1WIJeOl223TeqG5b6voxikiIt0lkba2yM7iYXXkSaXxYO4aftPGiaeuWe
+A9NORWZMupGMbCPdN0WBAmLWGM70Y6dZoS2p7znCqznfeVQvxnfMrmssmUxg/7pn9BC3CMlYTxnv
+W3XYnsDwy0LKQDboGlxKwGgY1jKgNVLesmydHS+jTRucZiw0mOupNrsL+jN5AnfYS45YQeYAD6Ps
+UPebCF9i/lu5NDJ9Ok492oSYMq4OO1/tZ2GiRr6mf5MoKduTd3n1Q/uOW7Jud7jaFaPCV7UfszzM
+pYExu701ONoMHCRQU6OpNkW5lvqh/XkmTrCyvM8rK4ElYRsL81e1koW61Oj46vEhIrOLOSqE/3vf
+cJyln31xD3dnNqdjbA7BYSUZ1TJfd1uF3Szty4hDrYYJ1TTi1tzrgj85hLF+sonyXpnLXLAYzKN2
+TQCgVr0uD2fFDy/mHMWjpbuEkfpSTIu3To327lxUFW/x6txVq98aOr5yhzOBMr6n2M5YCWdUmjDT
+hqt2V3aCjhS6gAph3C9cf7Dva9MuCp7ui8Cexl2XnhRHv2yxZ6k7So/R74rM9F5dR8vwjn3idVAS
+OFbOC6XwcvWIHiFL/RtC/Pn3m+8ZvgrNpRpQ3axwWRk3/JaFeseM8jLKsA+rF+q53OsIebnDbf75
+s89vZxncFIEA8PqLWnRk00kGuJF6itWySFpt6nOnV5XXufp6D31rEB1lSaajHjiFdiyFTL7zTJBa
+Q+JE7r7WMAOPMvTLcmOp7WdZADcHWo/YJsccMwDOqWkdLEHl/mfjMUc+3yhHmjYsr2RqJpnpmue8
+Juz3j29bL5syi5CcHJc1d461IQ/CbImdl3hrRE7GlyRyCLncS1JTPGi+cmu9NV6o+ILID+rvlKKD
+XmM3UKMwe+dgisj6mSDgwprslI+Z02ZcCNeCNYqVW2BbO59RtF7300kj7hlpWjLbJM4n6Ba4VZBX
+75S0q4N4poIwGWCpqr7Ff78dgImTOxl5yHddmsWreJXSM8YZIjvIOfUUKaHdgEGDt3TONQrzxrSz
+k7VdDh8fCsbR7hpZsJk6rQtuxFfMsYGn9DTwzpy8uDqQXdwiXaLHg0Tq9Wr5y3+Je0vb6OlOeiVU
+8Gg7nP/13stQFMgVlk7jB+lE0+q7FS3G4G4DrJY8/C3H+m5hkEfL6Xpv4H0ndKW+tSa3dbzHuvl0
+BpgB2msYXBOLULkjK/5Oi1BzlVxKoUG7bGSRxhbbAkBXCYFxSpYmAb/kQvUHiIGuujCcg8sRpac5
+rg==
